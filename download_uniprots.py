34838ffa1a47a3119b42d00710d0afeda02b3093
Griffin Calme <griffincalme@users.noreply.github.com>
2018-10-09 20:48:51 -0400

    Add files via upload

--- download_uniprots.py
+++ download_uniprots.py
@@ -1,20 +1,19 @@
 import urllib
 import pandas as pd
+import os
 
-df = pd.read_excel('../HUMAN_ATP_BINDING_Canon_Iso.fastauniprot.xlsx')
-
-
+df = pd.read_excel('HUMAN_ATP_BINDING_Canonical_AND_Isoforms.fastauniprot.xlsx')
 
 id_list = [x for x in df['Uniprot_ID']]
-
-
 
 no_isoform_list = [x for x in id_list if '-' not in x]
 
 print(id_list)
 print(no_isoform_list)
 
+os.makedirs('XML', exist_ok=True)
+
 for i in no_isoform_list:
-    urllib.request.urlretrieve('http://www.uniprot.org/uniprot/' + i + '.xml', i + '.xml')
+    urllib.request.urlretrieve('http://www.uniprot.org/uniprot/' + i + '.xml', 'XML/' + i + '.xml')
 
 
